72ee294452e63e1f6151839aaa2832d6a54beaf1
pkenil01 <patelkenil2312@gmail.com>
2025-11-08 14:03:24 -0500

    added routes to different pages

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+// Routes to different pages
+// Got assistance from GitHub Copilot, since i faced difficulty in creating routes for different pages
+
+const express = require('express');
+const router = express.Router();
+
+// Home page
+router.get('/', (req, res) => {
+    res.render('index', { title: 'Home' });
+});
+
+// About page
+router.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+});
+
+// Projects page
+router.get('/projects', (req, res) => {
+    res.render('projects', { title: 'Projects' });
+});
+
+// Contact page
+router.get('/contact', (req, res) => {
+    res.render('contact', { title: 'Contact' });
+});
+
+module.exports = router;
